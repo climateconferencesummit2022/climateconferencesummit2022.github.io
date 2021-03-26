4c326295112d0895e7e494b607e1355aee27e8cd
Glitch (tsa-webmaster-2021) <none>
2021-03-26 03:55:11 +0000

    🏪🎄 Checkpoint ./custom.js:28043158/7 ./chat.html:28043158/179 ./chat.css:28043158/208

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$('.chat_icon').click(function() {
+	$('.chat_icon').click(function(event) {
 		$('.chat_box').toggleClass('active');
 	});
 
-	$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
+	//$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
 });

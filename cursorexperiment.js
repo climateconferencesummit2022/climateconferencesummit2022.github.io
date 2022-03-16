14173682f78a003f1878d9ecc7c490a117a25e65
Glitch (tsa-webmaster-2021) <none>
2022-03-16 05:06:12 +0000

    👱👹 Checkpoint ./cursorexperiment.js:28043158/2790 ./cursorexperiment.css:28043158/1604 ./cursorexperiment.html:28043158/5916

--- cursorexperiment.js
+++ cursorexperiment.js
@@ -1,43 +1,28 @@
-(function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
-
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "09/30/",
-      birthday = dayMonth + yyyy;
-  
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  //end
-  
-  const countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {    
-
-        const now = new Date().getTime(),
-              distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-        //do something later when date is reached
-        if (distance < 0) {
-          document.getElementById("headline").innerText = "It's my birthday!";
-          document.getElementById("countdown").style.display = "none";
-          document.getElementById("content").style.display = "block";
-          clearInterval(x);
-        }
-        //seconds
-      }, 0)
-  }());
+<script>
+ // The data/time we want to countdown to
+ var countDownDate = new Date("Jul 25, 2021 16:37:52").getTime();
+ // Run myfunc every second
+ var myfunc = setInterval(function() {
+ var now = new Date().getTime();
+ var timeleft = countDownDate - now;
+ // Calculating the days, hours, minutes and seconds left
+ var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+ var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+ var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+ var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+ // Result is output to the specific element
+ document.getElementById("days").innerHTML = "J-"+ days +" "
+ document.getElementById("hours").innerHTML = hours + "h "
+ document.getElementById("mins").innerHTML = minutes + "m "
+ document.getElementById("secs").innerHTML = seconds + "s "
+ // Display the message when countdown is over
+ if (timeleft < 0) {
+ clearInterval(myfunc);
+ document.getElementById("days").innerHTML = ""
+ document.getElementById("hours").innerHTML = "" 
+ document.getElementById("mins").innerHTML = ""
+ document.getElementById("secs").innerHTML = ""
+ document.getElementById("counter-end").innerHTML = "Time's up!";
+ }
+ }, 10);
+</script>
